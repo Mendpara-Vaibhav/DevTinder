1a013298b9b770c945c65e9f2d7bf54a0f2bf80f
Mendpara-Vaibhav <mendparavaibhav@gmail.com>
2025-11-01 15:44:14 +0530

    setup Redux store

--- devTinder-FE/src/components/Login.jsx
+++ devTinder-FE/src/components/Login.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("vaibhav@gmail.com");
   const [password, setPassword] = useState("Vaibhav@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      dispatch(addUser(res.data));
+      navigate("/");
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
   };
 
