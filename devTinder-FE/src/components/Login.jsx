8c5326afb50eeca660018c8d96d86b7d44af37d0
Mendpara-Vaibhav <mendparavaibhav@gmail.com>
2025-11-02 12:03:40 +0530

    built logout feature

--- devTinder-FE/src/components/Login.jsx
+++ devTinder-FE/src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("vaibhav@gmail.com");
   const [password, setPassword] = useState("Vaibhav@123");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -24,7 +25,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       navigate("/");
     } catch (err) {
-      console.error(err);
+      setError(err?.response?.data || "Something went wrong!");
     }
   };
 
@@ -53,6 +54,7 @@ const Login = () => {
               />
             </fieldset>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
