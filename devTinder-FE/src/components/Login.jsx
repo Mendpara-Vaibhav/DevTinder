740f9372caefa02cc3cb4f5416a5d0788e08171f
Mendpara Vaibhav <mendparavaibhav@gmail.com>
2026-01-04 12:22:48 +0530

    comment remove

--- devTinder-FE/src/components/Login.jsx
+++ devTinder-FE/src/components/Login.jsx
@@ -7,8 +7,6 @@ import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
-  // vaibhav@gmail.com
-  // Vaibhav@123
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
